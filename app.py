5f2394db4ad9f8cb518cb2c446fd2a4f418e8489
hosackm <hosack.matt@gmail.com>
2016-04-12 15:58:12 -0700

    Adding camera route for displaying a frame grabbed from the camera

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from camera import Camera
 
 app = Flask(__name__)
+cam = Camera()
 
 
 @app.route("/")
@@ -8,13 +10,12 @@ def index():
     return render_template("index.html", status=True)
 
 
-# Use special routes for debugging
-@app.route("/status/<on_off>")
-def status(on_off):
-    if on_off == "1":
-        return render_template("index.html", status=True)
-    else:
-        return render_template("index.html", status=False)
+@app.route("/camera")
+def camera():
+    cam.grab_frame()
+    cam.save_frame("default.jpg")
+    return render_template("camera.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
