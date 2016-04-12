274ff3179c52eececef19155aebc0e88b6688f07
Matt Hosack <hosack.matt@gmail.com>
2016-04-12 14:35:10 -0700

    Adding bootstrap and images.  Adding base.html and index.html for the main page.

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", status=True)
+
+
+@app.route("/status/<on_off>")
+def status(on_off):
+    if on_off == "1":
+        return render_template("index.html", status=True)
+    else:
+        return render_template("index.html", status=False)
+
+if __name__ == "__main__":
+    app.run(debug=True)
